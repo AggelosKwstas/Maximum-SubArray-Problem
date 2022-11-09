c6f50caf5d7f6dd671845ed1bd559b6001ac91b5
unknown <agelos.kostas21@gmail.com>
2022-11-09 16:59:48 +0200

    updated tests

--- tests.py
+++ tests.py
@@ -4,6 +4,7 @@ from solution import brute_force, calculate_prefix, calculate_kadane
 
 class TestMethods(unittest.TestCase):
 
+
     def test_bruteforce(self):
         self.assertEqual(brute_force([-2, 1, -3, 4, -1, 2, 1, -5, 4]), (6, 3, 6))
         self.assertEqual(brute_force([-2, -3, 4, -1, -2, 1, 5, -3]), (7, 2, 6))
